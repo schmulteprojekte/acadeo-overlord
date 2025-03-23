25543082f4757b9dfcf0c4e5ec229b48a9914c13
emilrueh <emilrueh@googlemail.com>
2025-03-23 15:41:38 +0100

    Use simplified polling

--- src/utils/requests.py
+++ src/utils/requests.py
@@ -1,20 +1,20 @@
 import requests
 from typing import Literal
 
-from src.utils.decorators import poll_until_completed
+from src.utils.decorators import poll
 from src.utils.exceptions import NotCompleted
 
 
 def call_endpoint(endpoint: str, method: Literal["GET", "POST"] = "GET", data: dict = None) -> dict:
     response = getattr(requests, method.lower())(endpoint, json=data)
-    # logger.debug(f"{response.status_code} status from {endpoint}")
     return response.json()
 
 
-@poll_until_completed()
+@poll
 def get_result(endpoint: str):
     response = call_endpoint(endpoint, "GET")
 
     if response["status"] != "completed":
         raise NotCompleted
+
     return response["result"]
