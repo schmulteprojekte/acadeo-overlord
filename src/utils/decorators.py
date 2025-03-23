8bc9072124e456dff7266baced6a73bac0fba77d
emilrueh <emilrueh@googlemail.com>
2025-03-23 15:41:08 +0100

    Simplify polling

--- src/utils/decorators.py
+++ src/utils/decorators.py
@@ -4,19 +4,19 @@ from src.utils.loggers import logger
 from src.utils.exceptions import NotCompleted
 
 
-def poll_until_completed(attempts: int = 6, delay: int = 1):
-    def decorator(func):
-        def wrapper(*args, **kwargs):
-            retries = 0
-            while retries < attempts:
-                try:
-                    return func(*args, **kwargs)
-                except NotCompleted:
-                    retries += 1
-                    wait = delay * (2**retries - 1)
-                    logger.debug(f"Waiting for {wait}s before polling again with attempt {retries}")
-                    time.sleep(wait)
+def poll(func):
+    attempts = 6
+    delay = 1
 
-        return wrapper
+    def wrapper(*args, **kwargs):
+        retries = 0
+        while retries < attempts:
+            try:
+                return func(*args, **kwargs)
+            except NotCompleted:
+                retries += 1
+                wait = delay * (2**retries - 1)
+                logger.debug(f"Waiting for {wait}s before polling again with attempt {retries}")
+                time.sleep(wait)
 
-    return decorator
+    return wrapper
