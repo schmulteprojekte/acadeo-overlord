f345314ca7bf6f47d53759bbf2243ab2e3786bbd
emilrueh <emilrueh@googlemail.com>
2025-03-26 17:05:41 +0100

    Align with service renames and add gen_uuid

--- src/endpoints/services.py
+++ src/endpoints/services.py
@@ -1,21 +1,33 @@
 from fastapi import APIRouter
 from fastapi.concurrency import run_in_threadpool
 
+
 from src.core import sse
+
+from src.utils.helper import gen_uuid
+
 from src.schemas import AIRequest
-from src.services.ai import call_ai
+
+from src.services.ai import call_openai
 
 
 router = APIRouter()
 
 
-@router.post("/ai")
+@router.post("/call_openai")
 @sse.endpoint
-async def ai_request(request: AIRequest):
+async def endpoint_call_openai(request: AIRequest):
     # avoid blocking via threadpool
     response = await run_in_threadpool(
-        call_ai,
+        call_openai,
         request.messages,
         request.model,
     )
+    return response
+
+
+@router.post("/gen_uuid")
+@sse.endpoint
+async def endpoint_gen_uuid():
+    response = await run_in_threadpool(gen_uuid)
     return response
