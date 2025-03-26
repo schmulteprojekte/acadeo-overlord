d17cd7b3d5fa6ff5d2aa69fc75b78a026c58296c
emilrueh <emilrueh@googlemail.com>
2025-03-26 17:00:17 +0100

    Rename to standard main entrypoint

--- api.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-
-from src.endpoints import services
-
-
-app = FastAPI()
-
-app.include_router(services.router)
--- main.py
+++ main.py
@@ -1,37 +1,6 @@
-# from fastapi import FastAPI
-
-# from src.utils import call_ai
-# from src.schemas import UserPrompt
-
-# import random, time
+from fastapi import FastAPI
+from src.endpoints import services
 
 
-# app = FastAPI()
-
-
-# # @app.post("/prompt_ai")
-# # async def prompt_ai(request: UserPrompt):
-
-# #     # messages = []
-# #     # messages.append({"role": "user", "content": request.prompt})
-
-# #     # data = call_ai(messages, model="gpt-4o-mini")
-# #     data = {"reply": f"test{random.randint(1, 100)}"}
-# #     time.sleep(random.randint(1, 3))
-# #     return data
-
-
-# from fastapi.concurrency import run_in_threadpool
-
-
-# @app.post("/prompt_ai")
-# async def prompt_ai(request: UserPrompt):
-#     # foo = lambda: call_ai([{"role": "user", "content": request.prompt}], model="gpt-4o-mini")
-
-#     def foo():
-#         time.sleep(random.randint(1, 3))
-#         return {"reply": f"test{random.randint(1, 100)}"}
-
-#     # Run blocking call_ai in a thread pool
-#     data = await run_in_threadpool(foo)
-#     return data
+app = FastAPI()
+app.include_router(services.router)
