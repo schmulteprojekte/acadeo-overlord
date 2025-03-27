a1eb4fa908283bdb3f839784b8b17bf3aa87ffaa
emilrueh <emilrueh@googlemail.com>
2025-03-27 22:16:36 +0100

    Add root server status check endpoint

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from src.endpoints import services
 
 
 app = FastAPI()
 app.include_router(services.router)
+
+
+@app.get("/")
+def root():
+    return Response("Overlord is awake!")
