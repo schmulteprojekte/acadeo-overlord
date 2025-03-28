774bac5c30f5a3f00981771e0f533b6135ae74d0
emilrueh <emilrueh@googlemail.com>
2025-03-28 15:50:03 +0100

    Overhaul openai schema and draft gemini schema

--- src/endpoints/schemas.py
+++ src/endpoints/schemas.py
@@ -1,6 +1,41 @@
 from pydantic import BaseModel
 
 
-class AIRequest(BaseModel):
+class OpenAIRequest(BaseModel):
     messages: list
     model: str = "gpt-4o-mini"
+    temperature: float | None = 0.7
+    max_tokens: int | None = 4096
+    json_schema: type | bool | None = None
+
+
+class GeminiRequest(BaseModel):
+    "https://docs.litellm.ai/docs/providers/gemini"
+
+    messages: list
+    model: str = "gemini-2.0-flash"
+    temperature: float | None = 0.7
+    max_tokens: int | None = 4096
+    response_format: dict | None = None  # TODO
+    safety_settings: list[dict] = [
+        {
+            "category": "HARM_CATEGORY_HARASSMENT",
+            "threshold": "BLOCK_NONE",
+        },
+        {
+            "category": "HARM_CATEGORY_HATE_SPEECH",
+            "threshold": "BLOCK_NONE",
+        },
+        {
+            "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+            "threshold": "BLOCK_NONE",
+        },
+        {
+            "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+            "threshold": "BLOCK_NONE",
+        },
+        # {
+        #     "category": "HARM_CATEGORY_CIVIC_INTEGRITY",
+        #     "threshold": "BLOCK_NONE",
+        # },
+    ]
