1b551c1ba4afba9a639f95e349cf013a20377c46
emilrueh <emilrueh@googlemail.com>
2025-03-28 15:50:27 +0100

    Draft Gemini endpoint and use kwargs over args

--- src/endpoints/services.py
+++ src/endpoints/services.py
@@ -4,9 +4,9 @@ from fastapi.concurrency import run_in_threadpool
 
 from src.core import sse
 
-from src.utils.helper import gen_uuid
+from src.utils.helper import gen_uuid, handle_json_schema
 
-from src.endpoints.schemas import AIRequest
+from src.endpoints.schemas import OpenAIRequest, GeminiRequest
 
 from src.services.ai import call_litellm
 
@@ -14,14 +14,31 @@ from src.services.ai import call_litellm
 router = APIRouter()
 
 
-@router.post("/call_ai")
+@router.post("/call_openai")
 @sse.endpoint
-async def endpoint_call_openai(request: AIRequest):
-    # avoid blocking via threadpool
+async def endpoint_call_openai(request: OpenAIRequest):
     response = await run_in_threadpool(
         call_litellm,
-        request.messages,
-        request.model,
+        messages=request.messages,
+        model=request.model,
+        temperature=request.temperature,
+        max_completion_tokens=request.max_tokens,
+        response_format=handle_json_schema(request.json_schema),
+    )
+    return response
+
+
+@router.post("/call_gemini")
+@sse.endpoint
+async def endpoint_call_gemini(request: GeminiRequest):
+    response = await run_in_threadpool(
+        call_litellm,
+        messages=request.messages,
+        model=f"gemini/{request.model}",
+        temperature=request.temperature,
+        max_tokens=request.max_tokens,
+        response_format=None,  # TODO
+        safety_settings=request.safety_settings,
     )
     return response
 
