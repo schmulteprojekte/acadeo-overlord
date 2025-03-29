1dfa07292e4bb283ac1503369dae234aa4209cb6
emilrueh <emilrueh@googlemail.com>
2025-03-29 20:12:37 +0100

    Build prompt manager and handle langfuse integration server-side only

--- src/core/langfuse.py
+++ src/core/langfuse.py
@@ -1,17 +1,33 @@
-from langfuse.decorators import observe, langfuse_context
+from langfuse import Langfuse
+
+from src.utils.helper import handle_messages, handle_response_format
 
 
-def trace(func):
-    @observe
-    def wrapper(prompt, **placeholders):
-        langfuse_context.update_current_observation(prompt=prompt)
+class PromptManager:
+    lf = Langfuse()
 
-        return func(
-            messages=prompt.compile(**placeholders),
-            model=prompt.config.get("model"),
-            temperature=prompt.config.get("temperature"),
-            max_tokens=prompt.config.get("max_tokens"),
-            json_mode=bool(prompt.config.get("json_schema")),
-        )
+    @classmethod
+    def track(cls, func):
+        def wrapper(prompt: dict, placeholders: dict = None, metadata: dict = None):
+            langfuse_prompt = cls.lf.get_prompt(**prompt)
 
-    return wrapper
+            # ---
+            # TODO: refactor to pydantic model (attach to endpoint schemas):
+            params = dict(langfuse_prompt.config)  # HACK: weirdly needs to be re-cast to dict in order to work inside a decorator else throws RecursionError
+
+            # handle json schema before messages so it can be added to placeholders if only json mode instead of structured response
+            params["response_format"] = handle_response_format(langfuse_prompt, placeholders)
+
+            # handle messages
+            params["messages"] = handle_messages(langfuse_prompt, placeholders or {})
+
+            # add prompt as dict to metadata to enable prompt management
+            params["metadata"] = {"prompt": langfuse_prompt.__dict__}
+            # add custom metadata
+            if metadata:
+                params["metadata"]["custom"] = metadata
+            # ---
+
+            return func(**params)
+
+        return wrapper
