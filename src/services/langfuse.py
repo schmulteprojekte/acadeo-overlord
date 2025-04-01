bc4b4e1389074b959b34347fde400fc1bd429346
emilrueh <emilrueh@googlemail.com>
2025-04-01 22:59:23 +0200

    Re-build pydanctic model from json schema for structured responses to be standard json mode

--- src/services/langfuse.py
+++ src/services/langfuse.py
@@ -3,6 +3,8 @@ from langfuse import Langfuse
 from langfuse.model import PromptClient
 from pydantic import BaseModel
 
+from jsonschema_pydantic import jsonschema_to_pydantic
+
 
 lf = Langfuse()
 
@@ -10,16 +12,13 @@ lf = Langfuse()
 # HELPER
 
 
-def handle_response_format(prompt, placeholders):
-    json_schema = prompt.config.get("response_format")
-
-    if isinstance(json_schema, type):
-        # structured response
-        return json_schema
-    elif json_schema:
-        # json mode
-        placeholders["json_schema"] = json_schema
-        return {"type": "json_object"}
+def handle_response_format(json_schema):
+    if json_schema:
+        if isinstance(json_schema, dict):
+            return jsonschema_to_pydantic(json_schema)
+        elif isinstance(json_schema, str):
+            # return {"type", "json_object"}
+            raise Exception("Json mode is not supported! Please use structured responses.")
 
 
 def handle_messages(prompt: PromptClient, placeholders: dict = None):
@@ -39,14 +38,18 @@ class PromptConfig(BaseModel):
 
 def track(func):
     def wrapper(prompt_config: PromptConfig):
+        # get prompt from config and extract params
         langfuse_prompt = lf.get_prompt(**prompt_config.args)
         func_params = langfuse_prompt.config.copy()
 
-        # TODO: refactor to pydantic model
-        func_params["response_format"] = handle_response_format(langfuse_prompt, prompt_config.placeholders)
+        # pop schema from params and turn into model
+        json_schema = func_params.pop("json_schema", None)
+
+        # construct params from prompt
+        func_params["response_format"] = handle_response_format(json_schema)
         func_params["messages"] = handle_messages(langfuse_prompt, prompt_config.placeholders or {})
         func_params["metadata"] = {"prompt": langfuse_prompt}  # enable prompt management
-
+        # add custom metadata to generic from prompt
         if prompt_config.metadata:
             func_params["metadata"]["custom"] = prompt_config.metadata
 
