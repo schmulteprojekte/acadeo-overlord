c344649e4786944407f465796ee5811cd762b31b
emilrueh <emilrueh@googlemail.com>
2025-04-01 13:40:35 +0200

    Overhaul langfuse litellm endpoint

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ from fastapi.concurrency import run_in_threadpool
 
 from src.core import sse
 from src.services import langfuse, litellm
-from src.utils.helper import gen_uuid
+from src.utils import gen_uuid
 
 
 app = FastAPI()
+
+
+# HELPER
 
 
 @app.get("/")
@@ -22,12 +25,11 @@ async def _():
     return await run_in_threadpool(gen_uuid)
 
 
+# MAIN
+
+
 @app.post("/langfuse_litellm")
 @sse.endpoint
-async def _(request: langfuse.Request):
-    return await run_in_threadpool(
-        langfuse.track(litellm.call),
-        request.prompt_params,
-        request.prompt_placeholders,
-        request.metadata,
-    )
+async def _(request: langfuse.PromptConfig):
+    func, args = langfuse.track(litellm.call), request
+    return await run_in_threadpool(func, args)
