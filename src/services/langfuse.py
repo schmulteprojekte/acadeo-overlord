cbea5a20e69237bd539a7f2a67a3e6e1cfcfdb70
emilrueh <emilrueh@googlemail.com>
2025-04-01 13:39:00 +0200

    Move helper functions here and refactor for pydantic model prompt

--- src/services/langfuse.py
+++ src/services/langfuse.py
@@ -1,33 +1,55 @@
 from langfuse import Langfuse
+
+from langfuse.model import PromptClient
 from pydantic import BaseModel
-from src.utils.helper import handle_messages, handle_response_format
 
 
 lf = Langfuse()
 
 
-class Request(BaseModel):
-    prompt_params: dict
-    prompt_placeholders: dict | None = None
+# HELPER
+
+
+def handle_response_format(prompt, placeholders):
+    json_schema = prompt.config.get("response_format")
+
+    if isinstance(json_schema, type):
+        # structured response
+        return json_schema
+    elif json_schema:
+        # json mode
+        placeholders["json_schema"] = json_schema
+        return {"type": "json_object"}
+
+
+def handle_messages(prompt: PromptClient, placeholders: dict = None):
+    compiled_prompt = prompt.compile(**placeholders)
+    messages = compiled_prompt if isinstance(prompt.prompt, list) else [{"role": "user", "content": compiled_prompt}]
+    return messages
+
+
+# MAIN
+
+
+class PromptConfig(BaseModel):
+    args: dict
+    placeholders: dict | None = None
     metadata: dict | None = None
 
 
 def track(func):
-    def wrapper(prompt_params: dict, prompt_placeholders: dict = None, metadata: dict = None):
-        prompt = lf.get_prompt(**prompt_params)
+    def wrapper(prompt_config: PromptConfig):
+        langfuse_prompt = lf.get_prompt(**prompt_config.args)
+        func_params = langfuse_prompt.config.copy()
 
-        # --- TODO: refactor to pydantic model (attach to endpoint schemas)
+        # TODO: refactor to pydantic model
+        func_params["response_format"] = handle_response_format(langfuse_prompt, prompt_config.placeholders)
+        func_params["messages"] = handle_messages(langfuse_prompt, prompt_config.placeholders or {})
+        func_params["metadata"] = {"prompt": langfuse_prompt}  # enable prompt management
 
-        params = dict(prompt.config)  # HACK: weirdly needs to be re-cast to dict in order to work inside a decorator else throws RecursionError
-        params["response_format"] = handle_response_format(prompt, prompt_placeholders)
-        params["messages"] = handle_messages(prompt, prompt_placeholders or {})
-        params["metadata"] = {"prompt": prompt.__dict__}  # enable prompt management
+        if prompt_config.metadata:
+            func_params["metadata"]["custom"] = prompt_config.metadata
 
-        if metadata:
-            params["metadata"]["custom"] = metadata
-
-        # ---
-
-        return func(**params)
+        return func(**func_params)
 
     return wrapper
