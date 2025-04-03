1e49731e71c01a588b6c8042db1d869552294d96
emilrueh <emilrueh@googlemail.com>
2025-04-03 17:15:05 +0200

    Name endpoint

--- src/endpoints/ai.py
+++ src/endpoints/ai.py
@@ -10,6 +10,6 @@ langfuse_router = APIRouter(prefix="/langfuse")
 
 @langfuse_router.post("/litellm")
 @sse.endpoint
-async def _(request: langfuse.PromptConfig):
+async def langfuse_litellm(request: langfuse.PromptConfig):
     func, args = langfuse.track(litellm.call), request
     return await run_in_threadpool(func, args)
