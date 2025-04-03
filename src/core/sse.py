7777de6eda0e046913b345d7a1f475dba9488e7f
emilrueh <emilrueh@googlemail.com>
2025-04-03 09:19:32 +0200

    Split apart generator from decorator for readability

--- src/core/sse.py
+++ src/core/sse.py
@@ -3,18 +3,18 @@ from functools import wraps
 import json
 
 
+async def event_generator(result):
+    if result is None:
+        yield {"event": "error", "data": "result is None"}
+        return
+
+    yield {"event": "result", "data": json.dumps(result)}
+
+
 def endpoint(func):
     @wraps(func)
     async def wrapper(*args, **kwargs):
         result = await func(*args, **kwargs)
-
-        async def event_generator():
-            if result is None:
-                yield {"event": "error", "data": "result is None"}
-                return
-
-            yield {"event": "result", "data": json.dumps(result)}
-
-        return EventSourceResponse(event_generator())
+        return EventSourceResponse(event_generator(result))
 
     return wrapper
