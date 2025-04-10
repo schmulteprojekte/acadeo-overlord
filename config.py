5e4daf4d90898535d3f5c93d388a0d4025a3c60b
emilrueh <emilrueh@googlemail.com>
2025-04-10 19:20:34 +0200

    Default to overlord as name

--- config.py
+++ config.py
@@ -5,10 +5,11 @@ import litellm, json, os
 load_dotenv(dotenv_path=".env", override=True)
 
 
-name = os.getenv("APP_NAME")
+name = os.getenv("APP_NAME", "overlord")
 access_keys = json.loads(os.getenv("ACCESS_KEYS", "[]"))
 origins = json.loads(os.getenv("ALLOWED_ORIGINS", "[]"))
 rates = json.loads(os.getenv("RATE_LIMITS", "[]"))
 
 litellm.success_callback = ["langfuse"]
 litellm.failure_callback = ["langfuse"]
+# litellm._turn_on_debug()
