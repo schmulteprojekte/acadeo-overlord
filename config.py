fea64a354b6f70eb1e232821e0f582b95b62c9d0
emilrueh <emilrueh@googlemail.com>
2025-04-11 12:13:37 +0200

    Validate crucial configurations are set

--- config.py
+++ config.py
@@ -10,6 +10,12 @@ access_keys = json.loads(os.getenv("ACCESS_KEYS", "[]"))
 origins = json.loads(os.getenv("ALLOWED_ORIGINS", "[]"))
 rates = json.loads(os.getenv("RATE_LIMITS", "[]"))
 
+if not access_keys:
+    raise ValueError("No access keys are set!")
+if not rates:
+    raise ValueError("No rate limits are set!")
+
+
 litellm.success_callback = ["langfuse"]
 litellm.failure_callback = ["langfuse"]
 # litellm._turn_on_debug()
