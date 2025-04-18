038f00bb90e8a83a7f3599403edd781948a3f1c2
emilrueh <emilrueh@googlemail.com>
2025-04-18 11:01:20 +0200

    Cleanup dev comment

--- src/services/langfuse.py
+++ src/services/langfuse.py
@@ -46,13 +46,3 @@ class ClientManager:
 async def fetch_prompt(prompt_config: PromptConfig) -> PromptClient:
     lf = ClientManager.get_client(prompt_config.project)
     return await run_in_threadpool(lf.get_prompt, **prompt_config.args.model_dump())
-
-
-# I still feel like there is a limitation of using the litellm langfuse integration
-# as it requires to always send a langfuse prompt object to the metadata in order to
-# track it but what if I want to observe an external prompt via langfuse and call via
-# litellm e.g. in a chat bot scenario where only the inital prompt might be coming
-# from langfuse but the following from the user which then are linked via session id
-
-# what if the prompt config does not need to be sent as long as the inital generation
-# is linked to the following chat messages via the trace id?
