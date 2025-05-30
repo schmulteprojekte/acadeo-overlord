890966a5df75116285a0049ac545ae581ddfc365
emilrueh <emilrueh@googlemail.com>
2025-05-30 14:08:46 +0200

    Load new custom rate limits and validate

--- config.py
+++ config.py
@@ -8,12 +8,24 @@ load_dotenv(dotenv_path=".env", override=True)
 name = os.getenv("APP_NAME", "overlord")
 access_keys = json.loads(os.getenv("ACCESS_KEYS", "[]"))
 origins = json.loads(os.getenv("ALLOWED_ORIGINS", "[]"))
-rates = json.loads(os.getenv("RATE_LIMITS", "[]"))
 
 if not access_keys:
     raise ValueError("No access keys are set!")
-if not rates:
-    raise ValueError("No rate limits are set!")
+
+rate_limits_default = json.loads(os.getenv("RATE_LIMITS_DEFAULT", "[]"))
+rate_limits_high = json.loads(os.getenv("RATE_LIMITS_HIGH", "[]"))
+
+if not rate_limits_default:
+    raise ValueError("No default rate limits are set!")
+
+# Use default limits for high-usage if not specified
+if not rate_limits_high:
+    rate_limits_high = rate_limits_default
+
+rates = {
+    "default": rate_limits_default,
+    "high-usage": rate_limits_high,
+}
 
 
 litellm.success_callback = ["langfuse"]
